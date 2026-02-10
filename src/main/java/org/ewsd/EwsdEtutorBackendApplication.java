b4206e05bff743f16f659891e27d0e7f49793475
Aung The Ko <aung.tk@mit.com.mm>
2026-02-10 11:55:00 +0630

    Initial commit: Springboot setup add database config yaml file

---
+++ src/main/java/org/ewsd/EwsdEtutorBackendApplication.java
@@ -0,0 +1,13 @@
+package org.ewsd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwsdEtutorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EwsdEtutorBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ewsd/EwsdEtutorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ewsd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EwsdEtutorBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
