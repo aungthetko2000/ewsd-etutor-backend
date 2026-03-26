376a2194f5450fce06941d1eb5d452c968a7abf1
devHtut <dev.htutkhaung@gmail.com>
2026-03-26 21:35:13 +0630

    SCRUM-47:Analytics & Usage Reporting API

--- src/main/java/org/ewsd/repository/activity/UserActivityRepository.java
+++ src/main/java/org/ewsd/repository/activity/UserActivityRepository.java
@@ -1,23 +1,27 @@
 package org.ewsd.repository.activity;
 
+import org.ewsd.dto.analytics.ReportSummaryDTO;
 import org.ewsd.entity.activity.UserActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface UserActivityRepository extends JpaRepository<UserActivity, Long> {
 
-    @Query(value = "SELECT browser FROM user_activities GROUP BY browser ORDER BY COUNT(*) DESC LIMIT 1", nativeQuery = true)
-    String findMostUsedBrowser();
+    @Query("SELECT new org.ewsd.dto.analytics.ReportSummaryDTO$BrowserStat(u.browser, COUNT(u)) " +
+            "FROM UserActivity u WHERE u.timestamp >= :since GROUP BY u.browser ORDER BY COUNT(u) DESC")
+    List<ReportSummaryDTO.BrowserStat> findBrowserStats(LocalDateTime since);
 
-    @Query(value = "SELECT username FROM user_activities WHERE username IS NOT NULL GROUP BY username ORDER BY COUNT(*) DESC LIMIT 1", nativeQuery = true)
-    String findMostActiveUser();
+    @Query("SELECT new org.ewsd.dto.analytics.ReportSummaryDTO$PageStat(u.requestUri, COUNT(u)) " +
+            "FROM UserActivity u WHERE u.timestamp >= :since GROUP BY u.requestUri ORDER BY COUNT(u) DESC")
+    List<ReportSummaryDTO.PageStat> findPageStats(LocalDateTime since);
 
-    @Query(value = "SELECT browser, COUNT(*) FROM user_activities GROUP BY browser", nativeQuery = true)
-    List<Object[]> getBrowserUsageStats();
-
-    @Query(value = "SELECT username, COUNT(*) FROM user_activities WHERE username IS NOT NULL GROUP BY username ORDER BY COUNT(*) DESC LIMIT 5", nativeQuery = true)
-    List<Object[]> getTopActiveUsers();
+    @Query("SELECT new org.ewsd.dto.analytics.ReportSummaryDTO$UserStat(u.username, COUNT(u)) " +
+            "FROM UserActivity u WHERE u.timestamp >= :since AND u.username != 'Anonymous' " +
+            "GROUP BY u.username ORDER BY COUNT(u) DESC")
+    List<ReportSummaryDTO.UserStat> findActiveUsers(LocalDateTime since);
 }
